ead4414bce4439aef0a44aadf772474c34d7945d
Webl1sek <68706516+webl1sek@users.noreply.github.com>
2021-06-12 02:15:31 +0300

    Added several comments

--- douParser.py
+++ douParser.py
@@ -14,33 +14,38 @@ response = requests.get(url, headers=headers).text
 
 soup = BeautifulSoup(response, "lxml")
 
-categoriesDict = {}
-lowSkillVacancies = []
+categoriesDict = {} #Here category names and links are stored
+lowSkillVacancies = [] 
 resultingList = []
 
-totalVacanciesCount = soup.findAll("em")
 
-for i in range(len(totalVacanciesCount)): totalVacanciesCount[i] = int(totalVacanciesCount[i].getText())
+#Gets count of offers for each vacancy
+totalOffersCount = soup.findAll("em")
 
+for i in range(len(totalOffersCount)): totalOffersCount[i] = int(totalOffersCount[i].getText())
+
+
+#Gets names of vacancies and links to them, then incert it inside a dictionary
 catlink = soup.findAll(class_ = "cat-link")
 
 for i in range(len(catlink)):
     categoriesDict[catlink[i].text] = catlink[i].get('href') + "&exp=0-1"
 
-
+#Gets count of low skill vacancies   
 for i in categoriesDict.values():
     response = requests.get(i, headers=headers).text
     soup = BeautifulSoup(response, "lxml")
     num = soup.find("h1").text[0:2]
-    lowSkillVacancies.append(int(num) if num.isnumeric() else 0)
+    lowSkillVacancies.append(int(num) if num.isnumeric() and not "   " else 0)
 
-n = 0
+
 date = datetime.datetime.now().strftime("%d %B %Y")
 
+#Outputs the result of parsing
 print(f"Dou.ua vacancies dump for {date}")
-
+n = 0 # for iteration purpose :)
 for i in categoriesDict:
-    print(f" Count of newbie vacancies for {i} is {round(lowSkillVacancies[n] / totalVacanciesCount[n] * 100)}%")
+    print(f" Count of newbie vacancies for {i} is {round(lowSkillVacancies[n] / totalOffersCount[n] * 100)}%")
     n += 1
 
 
