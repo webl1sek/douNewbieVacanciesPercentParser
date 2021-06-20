71672aa9ce1aa9683c1dbd4545271b5f2784c4ec
Webl1sek <68706516+webl1sek@users.noreply.github.com>
2021-06-20 23:07:14 +0300

    Made some simplifications

--- douParser.py
+++ douParser.py
@@ -2,51 +2,49 @@
 
 from bs4 import BeautifulSoup
 import requests
-import datetime
 
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"
 }
 
-url = "https://jobs.dou.ua/"
-
-response = requests.get(url, headers=headers).text
+response = requests.get("https://jobs.dou.ua/", headers=headers).text
 
 soup = BeautifulSoup(response, "lxml")
 
-categoriesDict = {} #Here category names and links are stored
+vacancies = []
 lowSkillVacancies = [] 
-resultingList = []
-
 
 #Gets count of offers for each vacancy
-totalOffersCount = soup.findAll("em")
-
-for i in range(len(totalOffersCount)): totalOffersCount[i] = int(totalOffersCount[i].getText())
-
+offersCount = soup.findAll("em")
 
 #Gets names of vacancies and links to them, then incert it inside a dictionary
 catlink = soup.findAll(class_ = "cat-link")
 
-for i in range(len(catlink)):
-    categoriesDict[catlink[i].text] = catlink[i].get('href') + "&exp=0-1"
-
-#Gets count of low skill vacancies   
-for i in categoriesDict.values():
-    response = requests.get(i, headers=headers).text
+#Gets number of low skill requirements
+for link in catlink:
+    response = requests.get(link.get('href') + "&exp=0-1", headers=headers).text
     soup = BeautifulSoup(response, "lxml")
     num = soup.find("h1").text[0:2]
-    lowSkillVacancies.append(int(num) if num.isnumeric() and not "   " else 0)
+    lowSkillVacancies.append(int(num) if num.isnumeric() else 0)
 
 
-date = datetime.datetime.now().strftime("%d %B %Y")
+for i in range(len(catlink or offersCount)):
+    vacancies.append({
+        "name" : catlink[i].text,
+        "link" : catlink[i].get('href'),
+        "count" :  int(offersCount[i].getText()),
+        "newbieCount" : lowSkillVacancies[i], 
+        "percent" : round(lowSkillVacancies[i] / int(offersCount[i].getText()) * 100)
+    })
 
-#Outputs the result of parsing
-print(f"Dou.ua vacancies dump for {date}")
-n = 0 # for iteration purpose :)
-for i in categoriesDict:
-    print(f" Count of newbie vacancies for {i} is {round(lowSkillVacancies[n] / totalOffersCount[n] * 100)}%")
-    n += 1
+
+outputList = sorted(vacancies, key = lambda i: i['percent'], reverse=True)
+
+for i in range(len(outputList)):
+    print("Count of newbie vacancies for ", outputList[i]["name"], "is", outputList[i]["percent"], "%")
+
+
+
 
 
 
